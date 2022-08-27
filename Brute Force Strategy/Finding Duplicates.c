2832c0c36566128817c147e6b1d8e4bc1aeb0202
TechWriter03 <111000634+TechWriter03@users.noreply.github.com>
2022-08-27 21:15:59 +0530

    Create Finding Duplicates.c

---
+++ Brute Force Strategy/Finding Duplicates.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+                printf("%d",a[i]);
+        }
+    }
+    return 0;
+}
