1c32e0c333a0b640082b066d3b62771e3f8cfc53
TechWriter03 <111000634+TechWriter03@users.noreply.github.com>
2022-10-10 13:05:21 +0530

    Update Pair with Difference.c

--- Brute Force Strategy/Pair with Difference.c
+++ Brute Force Strategy/Pair with Difference.c
@@ -1,3 +1,30 @@
+/* Problem Statement:
+
+Given an array A of sorted integers and another non negative integer k, 
+find if there exists 2 indices i and j such that A[j] - A[i] = k, i != j.
+
+Input Format:
+First Line n - Number of elements in an array
+Next n Lines - N elements in the array
+k - Non - Negative Integer
+
+Output Format:
+1 - If pair exists
+0 - If no pair exists
+
+Explanation for the given Sample Testcase:
+YES as 5 - 1 = 4
+So Return 1.
+
+For Example:
+–——————————————————-
+| Input  | Result  |
+|————————|—————————|
+|3       |1        |
+|1 3 5   |         |
+|4       |         |
+|________|_________|  */
+
 #include<stdio.h>
 int main()
 {
