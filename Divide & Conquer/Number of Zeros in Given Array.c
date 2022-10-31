052bfe981f45a2f422ba032d1425d85f0b8427aa
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2022-10-31 17:12:01 +0530

    Update Number of Zeros in Given Array.c

--- Divide & Conquer/Number of Zeros in Given Array.c
+++ Divide & Conquer/Number of Zeros in Given Array.c
@@ -1,3 +1,14 @@
+/* Problem Statement:
+Given an array of 1s and 0s this has all 1s first followed by all 0s. Aim is to find the number of 0s.
+Write a program using Divide and Conquer to Count the number of zeroes in the given array.
+
+Input Format:
+- First Line Contains Integer m – Size of array
+- Next m lines Contains m numbers – Elements of an array
+
+Output Format:
+- First Line Contains Integer – Number of zeroes present in the given array. */
+
 #include<stdio.h>
 int divide(int n,int a[])
 {
