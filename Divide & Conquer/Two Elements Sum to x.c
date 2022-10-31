1178fd14fd9a7d38beea64e66303c1bbc61455c1
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2022-10-31 17:15:10 +0530

    Update Two Elements Sum to x.c

--- Divide & Conquer/Two Elements Sum to x.c
+++ Divide & Conquer/Two Elements Sum to x.c
@@ -1,3 +1,19 @@
+/*Problem Statement:
+Given a sorted array of integers say arr[] and a number x. Write a recursive program using divide and conquer 
+strategy to check if there exist two elements in the array whose sum = x. 
+If there exist such two elements then return the numbers, otherwise print as “No”.
+
+Note: Write a Divide and Conquer Solution.
+
+Input Format:
+   First Line Contains Integer n – Size of array
+   Next n lines Contains n numbers – Elements of an array
+   Last Line Contains Integer x – Sum Value
+   
+Output Format
+   First Line Contains Integer – Element1
+   Second Line Contains Integer – Element2 (Element 1 and Elements 2 together sums to value “x”) */
+
 #include<stdio.h>
 int main()
 {
